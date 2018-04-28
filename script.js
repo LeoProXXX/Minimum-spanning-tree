c0a6df1ac223aa82ea4fa3bfaee9a8cbec32096d
LeoProXXX <michal3184@vp.pl>
2018-04-28 20:09:04 +0200

    Remove unnecessary code

--- script.js
+++ script.js
@@ -1,50 +1,3 @@
-// Queue class
-class Queue {
-    // Array is used to implement a Queue
-    constructor() {
-        this.items = [];
-    }
-
-    // enqueue function
-    enqueue(element) {
-        // adding element to the queue
-        this.items.push(element);
-    }
-
-    // dequeue function
-    dequeue() {
-        // removing element from the queue
-        // returns underflow when called 
-        // on empty queue
-        if (this.isEmpty())
-            return "Underflow";
-        return this.items.shift();
-    }
-
-    // front function
-    front() {
-        // returns the Front element of 
-        // the queue without removing it.
-        if (this.isEmpty())
-            return "No elements in Queue";
-        return this.items[0];
-    }
-
-    // isEmpty function
-    isEmpty() {
-        // return true if the queue is empty.
-        return this.items.length == 0;
-    }
-
-    // printQueue function
-    printQueue() {
-        var str = "";
-        for (var i = 0; i < this.items.length; i++)
-            str += this.items[i] + " ";
-        return str;
-    }
-}
-
 // cretae a graph class
 class Graph {
     // defining vertex array and
@@ -59,13 +12,6 @@ class Graph {
                 this.AdjMatrix[i][j] = 0;
             }
         }
-    }
-
-    // add vertex to the graph
-    addVertex(v) {
-        // initialize the adjacent list with a
-        // null array
-        this.AdjList.set(v, []);
     }
 
     // add edge to the graph
@@ -83,158 +29,6 @@ class Graph {
 
     }
 
-    // Prints the vertex and adjacency list
-    printGraph() {
-        // get all the vertices
-        var get_keys = this.AdjList.keys();
-
-        // iterate over the vertices
-        for (var i of get_keys) {
-            // great the corresponding adjacency list
-            // for the vertex
-            var get_values = this.AdjList.get(i);
-            var conc = "";
-
-            // iterate over the adjacency list
-            // concatenate the values into a string
-            for (var j of get_values)
-                conc += j + " ";
-
-            // print the vertex and its adjacency list
-            console.log(i + " -> " + conc);
-        }
-    }
-
-    // function to performs BFS
-    bfs(startingNode) {
-
-        // create a visited array
-        var visited = [];
-        for (var i = 0; i < this.noOfVertices; i++)
-            visited[i] = false;
-
-        // Create an object for queue
-        var q = new Queue();
-
-        // add the starting node to the queue
-        visited[startingNode] = true;
-        q.enqueue(startingNode);
-
-        // loop until queue is element
-        while (!q.isEmpty()) {
-            // get the element from the queue
-            var getQueueElement = q.dequeue();
-
-            // passing the current vertex to callback funtion
-            console.log(getQueueElement);
-
-            // get the adjacent list for current vertex
-            var get_List = this.AdjList.get(getQueueElement);
-
-            // loop through the list and add the elemnet to the
-            // queue if it is not processed yet
-            for (var i in get_List) {
-                var neigh = get_List[i];
-
-                if (!visited[neigh]) {
-                    visited[neigh] = true;
-                    q.enqueue(neigh);
-                }
-            }
-        }
-    }
-
-    // main dfs method
-    dfs(startingNode) {
-
-        var visited = [];
-        for (var i = 0; i < this.noOfVertices; i++)
-            visited[i] = false;
-
-        this.DFSUtil(startingNode, visited);
-    }
-
-    // Recursive function which process and explore
-    // all the adjacent vertex of the vertex with which it is called
-    DFSUtil(vert, visited) {
-        visited[vert] = true;
-        console.log(vert);
-
-        var get_neighbours = this.AdjList.get(vert);
-
-        for (var i in get_neighbours) {
-            var get_elem = get_neighbours[i];
-            if (!visited[get_elem])
-                this.DFSUtil(get_elem, visited);
-        }
-    }
-
-    listToMatrix() {
-
-        var get_keys = this.AdjList.keys();
-
-        var arr = {};
-        for (var i of get_keys) {
-            var get_values = this.AdjList.get(i);
-            arr[i] = {};
-            for (var j of get_values) {
-                arr[i][j] = 1;
-            }
-        }
-
-        return arr;
-
-        /*
-                var ver = ['A', 'B', 'C', 'D', 'E', 'F'];
-                var value = [1, 2, 3, 4, 5];
-        
-                var arr = {};
-                for (var v in ver) {
-                    arr[ver[v]] = {};
-                    arr[ver[v]][ver[v]] = value[v];
-                }
-        
-                console.log(arr['A']['A']);
-        
-                var get_keys = this.AdjList.keys();
-        
-                // iterate over the vertices
-                for (var i of get_keys) {
-                    // great the corresponding adjacency list
-                    // for the vertex
-                    var get_values = this.AdjList.get(i);
-                    //var conc = "";
-        
-                    // iterate over the adjacency list
-                    // concatenate the values into a string
-                    for (var j of get_values)
-                        edges.push({
-                            from: i,
-                            to: j
-                        });
-                }
-        
-        
-        
-                for (var j of get_values) { // for(of) bierze elementy
-                    var exist = false;
-                    for (var k in edges) { // for(in) bierze indeksy
-                        if (edges[k]["from"] == j && edges[k]["to"] == i) {
-                            exist = true;
-                            break;
-                        }
-                    }
-                    if (!exist) {
-                        edges.push({
-                            from: i,
-                            to: j
-                        });
-                        //}
-                    }
-                }
-                */
-    }
-
     getVertices() {
         var nodes = [];
 
@@ -244,15 +38,6 @@ class Graph {
                 label: String(v)
             });
         }
-
-        // var nodeCount = this.AdjList.keys();
-        // for (var i of nodeCount) {
-        //     // for (var i = 0; i < nodeCount; i++) {
-        //     nodes.push({
-        //         id: i,
-        //         label: String(i)
-        //     });
-        // }
 
         return nodes;
     }
